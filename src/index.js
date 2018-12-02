a2e17b1c81fff9e7cfbaefed418a0c492fafb653
Emmanuel Ndukwe <emasysnd@gmail.com>
2018-12-02 11:59:01 +0100

    create boilerplate for graphql-psql

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import 'dotenv/config';
 import express from 'express';
 import log from 'fancy-log';
 import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
 import resolvers from './resolvers';
 import schema from './schema';
-import models from './fakeDb';
+import models, { sequelize } from './models';
 
 const app = express();
 app.use(cors());
@@ -14,12 +15,17 @@ const server = new ApolloServer({
   resolvers,
   context: {
     models,
-    me: models.users[1],
+    // me: models.users[1],
   },
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-app.listen({ port: 8000 }, () => {
-  log('Apollo Server on http://localhost:8000/graphql');
-});
+const init = async () => {
+  sequelize.sync();
+  await app.listen({ port: 8000 }, () => {
+    log('Apollo Server on http://localhost:8000/graphql');
+  });
+};
+
+init();
